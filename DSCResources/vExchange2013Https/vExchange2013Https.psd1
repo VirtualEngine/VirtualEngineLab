407a5a58c3af6bfd76f1bf45176de4110d081f56
iainbrighton <iainbrighton@hotmail.com>
2016-02-06 21:16:00 +0000

    Adds vExchange2013Https resource

---
+++ DSCResources/vExchange2013Https/vExchange2013Https.psd1
@@ -0,0 +1,9 @@
+@{
+    RootModule = 'vExchange2013Https.schema.psm1';
+    ModuleVersion = '1.0';
+    GUID = 'c0b782d9-8779-4b15-80ac-dfb52e1a4ca5';
+    Author = 'Iain Brighton';
+    CompanyName = 'Virtual Engine';
+    Copyright = '(c) 2016 Virtual Engine Limited. All rights reserved.';
+    Description = 'Virtual Engine Exchange 2013 server HTTPS installation DSC composite resource.';
+}
---
+++ DSCResources/vExchange2013Https/vExchange2013Https.schema.psm1
@@ -0,0 +1,75 @@
+configuration vExchange2013 {
+    param (
+        ## AD Schema Admin/Enterprise Admin credential
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential] $Credential,
+        
+        ## Path to Exchange 2013 setup.exe
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $Path,
+        
+        ## Path to Unified Communications Managed API 4 exe
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $UCMAPath,
+        
+        ## Exchange organization name
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $OrganizationName,
+        
+        # Personal information exchange (Pfx) ertificate file path
+        [Parameter(Mandatory)]
+        [System.String] $PfxCertificatePath,
+        
+        ## Pfx certificate thumbprint
+        [Parameter(Mandatory)]
+        [System.String] $PfxCertificateThumbprint,
+        
+        ## Pfx certificate password
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential] $PfxCertificateCredential
+    )
+
+    Import-DscResource -Module xPSDesiredStateConfiguration, xPendingReboot, xExchange;
+    ## Avoid recursive loading of the VirtualEngineLab composite module
+    Import-DscResource -Name vExchange2013Prerequisites, vExchange2013ADPrep;
+
+    vExchange2013Prerequisites ExchangePrerequisites {
+        UCMAPath = $UCMAPath;
+    }
+
+    vExchange2013ADPrep ExchangeADPrep {
+        Path = $Path;
+        OrganizationName = $OrganizationName;
+        Credential = $Credential;
+    }
+
+    xPendingReboot PendingRebootPreInstall {
+        Name = 'PreExchangeInstall';
+        DependsOn = '[vExchange2013Prerequisites]ExchangePrerequisites','[vExchange2013ADPrep]ExchangeADPrep';
+    }
+
+    xPackage ExchangeInstall {
+        Name = 'Exchange 2013';
+        ProductId = '{4934D1EA-BE46-48B1-8847-F1AF20E892C1}';
+        Path = $Path;
+        Arguments = '/mode:Install /role:Mailbox,ClientAccess /Iacceptexchangeserverlicenseterms';
+        RunAsCredential = $Credential;
+        DependsOn = '[vExchange2013Prerequisites]ExchangePrerequisites','[vExchange2013ADPrep]ExchangeADPrep','[xPendingReboot]PendingRebootPreInstall';
+    }
+        
+    xPendingReboot PendingRebootPostInstall {
+        Name = 'PostInstall';
+        DependsOn = '[xPackage]ExchangeInstall'
+    }
+    
+    xExchExchangeCertificate ExchangeCertificate {
+        Thumbprint = $PfxCertificateThumbprint;
+        Credential = $Credential;
+        CertCreds = $PfxCertificateCredential;
+        CertFilePath = $PfxCertificatePath;
+        Services = 'IIS';
+        Ensure = 'Present';
+        DependsOn = '[xPackage]ExchangeInstall';
+    }
+
+} #end configuration vExchange2013
