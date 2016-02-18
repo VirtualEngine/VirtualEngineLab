9f23aa1bdd0e71286d9f7cc1db223ca98e330fc8
Iain Brighton <iainbrighton@hotmail.com>
2016-02-18 17:10:00 +0000

    Adds vAccessDBEngine2010 resource

---
+++ DSCResources/vAccessDBEngine2010/vAccessDBEngine2010.psd1
@@ -0,0 +1,9 @@
+@{
+    RootModule = 'vAccessDBEngine2010.schema.psm1';
+    ModuleVersion = '1.0';
+    GUID = '6e81f0b8-70c2-495b-8d72-5616a335597f';
+    Author = 'Iain Brighton';
+    CompanyName = 'Virtual Engine';
+    Copyright = '(c) 2016 Virtual Engine Limited. All rights reserved.';
+    Description = 'Virtual Engine Microsoft Access Database Engine 2010 Redistributable DSC composite resource.';
+}
---
+++ DSCResources/vAccessDBEngine2010/vAccessDBEngine2010.schema.psm1
@@ -0,0 +1,19 @@
+configuration vAccessDBEngine2010 {
+    param (
+        ## Path to Microsoft Access Database Engine 2010 Redistributable installation exe
+        [Parameter(Mandatory)] [ValidateNotNull()]
+        [System.String] $Path
+    )
+ 
+    # Import the module that defines custom resources
+    Import-DscResource -Module xPSDesiredStateConfiguration;
+
+    xPackage 'MSAccessDBEngine2010' {
+        Name = 'Microsoft Access Database Engine 2010 Redistributable';
+        ProductId = '90140000-00D1-0409-1000-0000000FF1CE';
+        Path = $Path;
+        Arguments = '/quiet';
+        ReturnCode = 0;
+    }
+
+} #end configuration vAccessDBEngine2010
