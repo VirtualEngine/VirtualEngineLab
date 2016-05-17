9761286825db7182e6bfc5472ba32ee8b4888ef1
Iain Brighton <iainbrighton@hotmail.com>
2016-05-17 15:29:41 +0100

    Adds vScheduledTask

---
+++ DSCResources/vScheduledTask/vScheduledTask.psd1
@@ -0,0 +1,9 @@
+@{
+    RootModule = 'vScheduledTask.schema.psm1';
+    ModuleVersion = '1.0';
+    GUID = 'b5befeed-3a98-41d8-8fd3-090c128e18b3';
+    Author = 'Iain Brighton';
+    CompanyName = 'Virtual Engine';
+    Copyright = '(c) 2016 Virtual Engine Limited. All rights reserved.';
+    Description = 'Virtual Engine scheduled task DSC composite resource.';
+}
---
+++ DSCResources/vScheduledTask/vScheduledTask.schema.psm1
@@ -0,0 +1,50 @@
+configuration vScheduledTask {
+    param (
+        ## Scheduled task name
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $TaskName,
+        
+        ## Task scheduler folder/path
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $TaskPath,
+        
+        ## Target scheduled task state 
+        [Parameter(Mandatory)] [ValidateSet('Enabled','Disabled')]
+        [System.String] $State
+    )
+    
+    ## Requires Server 2012/Windows 8 or later
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration;
+    
+    $scriptResourceId = $TaskName.Replace(' ','');
+    Script $scriptResourceId {
+        
+        GetScript = {
+            $task = Get-ScheduledTask -TaskName $using:TaskName -TaskPath $using:TaskPath;
+            return @{ Target = $task.State; }
+        }
+        
+        TestScript = {
+            $task = Get-ScheduledTask -TaskName $using:TaskName -TaskPath $using:TaskPath;
+            if ($using:State -eq 'Enabled') {
+                ## Task could also be Ready or Queued
+                return ($task.State -ne 'Disabled');
+            }
+            else {
+                return ($task.State -eq $using:State);
+            }
+        }
+        
+        GetScript = {
+            if ($using:State -eq 'Enabled') {
+                [ref] $null = Enable-ScheduledTask -TaskName $using:TaskName -TaskPath $using:TaskPath;
+            }
+            else {
+                [ref] $null = Disable-ScheduledTask -TaskName $using:TaskName -TaskPath $using:TaskPath;
+            }
+        }
+    
+    } #end script
+    
+} #end configuration vScheduledTask
