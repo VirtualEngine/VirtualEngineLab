022ea6e19283ce52d11e9d11588354d9bdf6d5de
Iain Brighton <iainbrighton@hotmail.com>
2016-05-17 15:30:37 +0100

    Adds Win 7/2008 R2 support to vWorkgroupMember

--- DSCResources/vWorkgroupMember/vWorkgroupMember.schema.psm1
+++ DSCResources/vWorkgroupMember/vWorkgroupMember.schema.psm1
@@ -24,34 +24,68 @@ configuration vWorkgroupMember {
         [System.String] $DnsServer
     )
 
-    Import-DscResource -ModuleName xComputerManagement, xNetworking;
-
-    if ($IPAddress) {
-        xIPAddress IP {
-            IPAddress = $IPAddress;
-            SubnetMask = $SubnetMask;
-            InterfaceAlias = $InterfaceAlias;
-            AddressFamily = $AddressFamily;
+    Import-DscResource -ModuleName xComputerManagement, xNetworking, LegacyNetworking;
+    
+    if ($InterfaceAlias -match 'Local Area Connection') {
+        
+        ## Uses the legacy WMI calls
+        if ($DnsServer) {
+            vDNSServerAddress 'DNS' {
+                InterfaceAlias = $InterfaceAlias;
+                Address = $DnsServer;
+                AddressFamily = $AddressFamily;
+            }
         }
-    }
-
-    if ($DefaultGateway) {
-        xDefaultGatewayAddress Gateway {
-            Address = $DefaultGateway;
-            InterfaceAlias = $InterfaceAlias;
-            AddressFamily = $AddressFamily;
+        
+        if ($IPAddress) {
+            vIPAddress 'IP' {
+                IPAddress = $IPAddress;
+                SubnetMask = $SubnetMask;
+                InterfaceAlias = $InterfaceAlias;
+                AddressFamily = $AddressFamily;
+            }
         }
-    }
-
-    if ($DnsServer) {
-        xDNSServerAddress DNS {
-            InterfaceAlias = $InterfaceAlias;
-            Address = $DnsServer;
-            AddressFamily = $AddressFamily;
+        
+        if ($DefaultGateway) {
+            vDefaultGatewayAddress 'Gateway' {
+                Address = $DefaultGateway;
+                InterfaceAlias = $InterfaceAlias;
+                AddressFamily = $AddressFamily;
+            }
         }
-    }
+        
+    } #end InterfaceAlias like Local Area Connection
+    else {
+        
+        if ($DnsServer) {
+            xDNSServerAddress 'DNS' {
+                InterfaceAlias = $InterfaceAlias;
+                Address = $DnsServer;
+                AddressFamily = $AddressFamily;
+            }
+        }
+        
+        if ($IPAddress) {
+            xIPAddress 'IP' {
+                IPAddress = $IPAddress;
+                SubnetMask = $SubnetMask;
+                InterfaceAlias = $InterfaceAlias;
+                AddressFamily = $AddressFamily;
+            }
+        }
+
+        if ($DefaultGateway) {
+            xDefaultGatewayAddress 'Gateway' {
+                Address = $DefaultGateway;
+                InterfaceAlias = $InterfaceAlias;
+                AddressFamily = $AddressFamily;
+            }
+        }
+
+    } #end InterfaceAlias like Ethernet
 
     xComputer ComputerName {
         Name = $ComputerName;
     }
+
 }
