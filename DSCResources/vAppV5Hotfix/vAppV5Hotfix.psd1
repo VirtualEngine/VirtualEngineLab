69bcca751e20c029121d97bf0aff9d78d4ddb449
Iain Brighton <iainbrighton@hotmail.com>
2016-05-20 18:05:04 +0100

    Adds vAppV5Hotfix resource

---
+++ DSCResources/vAppV5Hotfix/vAppV5Hotfix.psd1
@@ -0,0 +1,9 @@
+@{
+    RootModule = 'vAppV5Hotfix.schema.psm1';
+    ModuleVersion = '1.0';
+    GUID = '0c7e0849-1379-4d56-bd76-4952c3c8deb7';
+    Author = 'Iain Brighton';
+    CompanyName = 'Virtual Engine';
+    Copyright = '(c) 2016 Virtual Engine Limited. All rights reserved.';
+    Description = 'Virtual Engine App-V 5.1 Hotfix Installation DSC composite resource.';
+}
---
+++ DSCResources/vAppV5Hotfix/vAppV5Hotfix.schema.psm1
@@ -0,0 +1,30 @@
+configuration vAppV5Hotfix {
+    param (
+        ## Path to App-V 5.1 hotfix installation .exe
+        [Parameter(Mandatory)] [ValidateNotNull()]
+        [System.String] $Path,
+        
+        [Parameter(Mandatory)] [ValidateNotNullOrEmpty()]
+        [System.String] $ProductId,
+        
+        [Parameter()]
+        [System.Boolean] $EnablePackageScripts = $true,
+        
+        [Parameter()] [ValidateSet('Present', 'Absent')]
+        [System.String] $Ensure = 'Present'
+    )
+ 
+    # Import the module that defines custom resources
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration;
+     
+    $resourceId = $ProductId.Replace('{','').Replace('}','').Replace('-','');
+    xPackage $resourceId {
+        Name = 'Microsoft Application Virtualization (App-V) Client';
+        Path = $Path;
+        ProductId = $ProductId;
+        Arguments = '/ACCEPTEULA=1 /CEIPOPTIN=0 /ENABLEPACKAGESCRIPTS={0} /NORESTART /q' -f ([System.Int32]$EnablePackageScripts);
+        ReturnCode = 0, 3010;
+        Ensure = $Ensure;
+    }
+
+} #end configuration vAppV5
